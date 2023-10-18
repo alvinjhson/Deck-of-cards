d45d3db78c9f02f42ca6802a45a23c95a46ef7ff
alvinjhson <alvinbest26@gmail.com>
2023-10-18 15:40:25 +0200

    Now you can compare cards

--- src/Card.java
+++ src/Card.java
@@ -1,21 +1,60 @@
+import java.util.Scanner;
+
 public class Card {
-    private int cardRank;
-    private String symbol;
+    Scanner sc = new Scanner(System.in);
+    private int type,value;
+    private String[] cardSymbol = {"Clubs","Spades","Diamonds","Hearts"};
+    private String[] cardRank = {"Ace","king","Queen","Jack","10","9","8","7","6","5","4","3"
+            ,"2"};
 
-    public Card(int cardRank, String symbol) {
-        this.cardRank = cardRank;
-        this.symbol = symbol;
+    public Card(int type, int value) {
+        this.type = type;
+        this.value = value;
     }
 
-    public int getCardRank() {
-        return cardRank;
+    public void setType(int type) {
+        this.type = type;
     }
 
-    public String getSymbol() {
-        return symbol;
+    public void setValue(int value) {
+        this.value = value;
     }
 
+    public int getType() {
+        return type;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void compareCards() {
+        Card card1 = new Card(0,0);
+        Card card2 = new Card(0,0);
+
+        System.out.println("Pick the first card");
+        int card1Rank = sc.nextInt();
+        card1.setValue(card1Rank);
+        int card1Symbol = sc.nextInt();
+        card1.setType(card1Symbol);
+        System.out.println(card1);
+        System.out.println("Pick the Second card");
+        int card2Rank = sc.nextInt();
+        card2.setValue(card2Rank);
+        int card2Symbol = sc.nextInt();
+        card2.setType(card2Symbol);
+        System.out.println(card2);
+
+        if (card1.getValue() + card1.getType() == card2.getValue() + card2.getType()){
+            System.out.println("These cards are the same");
+        }else System.out.println("Not the same cards");
+
+
+    }
+
+@Override
     public String toString(){
-        return "The card is " + getSymbol() + getCardRank();
+        String finalCard = cardRank[value] + " of " + cardSymbol[type];
+        return finalCard;
     }
 }
